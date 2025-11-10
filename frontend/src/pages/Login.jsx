c941108480cdd69e02bef2b132553c1de90da986
mishthimahajan <mishthimahajan0@gmail.com>
2025-11-10 20:40:57 +0530

    Update login API endpoint to production URL

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -338,7 +338,7 @@ const {storeTokenInLS} = useAuth();
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch('http://localhost:3000/login', {
+      const res = await fetch('https://skillexchange-2cap.onrender.com/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body:JSON.stringify(formData)
